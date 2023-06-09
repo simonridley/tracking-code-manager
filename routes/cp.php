54b85f642b2fdb380d9846376e438a90a9810604
Christian <hhhc@me.com>
2023-06-09 20:30:09 +0200

    Statamic v4 compatibility

--- routes/cp.php
+++ routes/cp.php
@@ -1,6 +1,6 @@
 <?php
 
 Route::middleware('web')->group(function () {
-    Route::get('/simonridley/trackingcodemanager/', 'TrackingCodeManagerController@index')->name('simonridley.tracking-code-manager.index');
-    Route::post('/simonridley/trackingcodemanager/', 'TrackingCodeManagerController@update')->name('simonridley.tracking-code-manager.update');
+    Route::get('/simonridley/trackingcodemanager/', '\Simonridley\TrackingCodeManager\Http\Controllers\TrackingCodeManagerController@index')->name('simonridley.tracking-code-manager.index');
+    Route::post('/simonridley/trackingcodemanager/', '\Simonridley\TrackingCodeManager\Http\Controllers\TrackingCodeManagerController@update')->name('simonridley.tracking-code-manager.update');
 });
