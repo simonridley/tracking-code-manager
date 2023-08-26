2d340d12bcb5750558512b316e151078aa2a5ad0
Ugur Aydogdu <ugur.aydogdu@epigra.com>
2023-08-26 20:40:56 +0100

    Update cp.php routes to have the right middleware
    fixes a bug that logs the user out because of the middleware

--- routes/cp.php
+++ routes/cp.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::middleware('web')->group(function () {
+Route::middleware('statamic.cp.authenticated')->group(function () {
     Route::get('/simonridley/trackingcodemanager/', '\Simonridley\TrackingCodeManager\Http\Controllers\TrackingCodeManagerController@index')->name('simonridley.tracking-code-manager.index');
     Route::post('/simonridley/trackingcodemanager/', '\Simonridley\TrackingCodeManager\Http\Controllers\TrackingCodeManagerController@update')->name('simonridley.tracking-code-manager.update');
 });
